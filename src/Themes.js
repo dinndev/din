b83f9115cccf77c55a645d8618f933123c02f497
dinndev <dinndev@gmail.com>
2021-02-03 19:57:31 +0800

    Object themes added

---
+++ src/Themes.js
@@ -0,0 +1,10 @@
+export const lightTheme = {
+  background:
+    "linear-gradient(116.22deg, #FBFBFB 26.65%, rgba(251, 251, 251, 0) 84.58%)",
+  color: "#666565",
+};
+
+export const darkTheme = {
+  background: "#666565",
+  color: "white",
+};
