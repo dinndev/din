f1ddbcf3ba32f0ea6c3d8f90ed271d5ef0d0c66b
dinndev <dinndev@gmail.com>
2021-02-09 17:52:00 +0800

    Dark mode added

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,14 +4,31 @@ import { ReactComponent as HtmlLogo } from "../Svg/html.svg";
 import { ReactComponent as JsLogo } from "../Svg/JS.svg";
 import { ReactComponent as TailwindLogo } from "../Svg/tailwind.svg";
 import { ReactComponent as ReactLogo } from "../Svg/React.svg";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { animateShow, hide } from "../Themes";
 
 export const Heading = styled.h1`
   text-align: center;
-  font-family: "Nunito", sans-serif;
-  opacity: ${({ animate }) => (animate ? animate.visibility : 0)};
+  transition: opacity 1s ease, transform 0.8s ease-in-out;
+  opacity: ${({ animate }) => (animate ? animate.visibility : 1)};
   transform: ${({ animate }) =>
-    animate ? `translateY(${animate.move})` : "translateY(20px)"};
-  transition: opacity 1s ease, transform 0.7s ease-in-out;
+    animate ? `translateY(${animate.move})` : "translateY(0px)"};
+  .show-enter {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  .show-enter-active {
+    opacity: 1;
+    transform: translateY(0px);
+    transition: opacity 1s ease, transform 0.7s ease-in-out;
+  }
+  .show-exit {
+    opacity: 1;
+  }
+  .show-exit-active {
+    opacity: 0;
+    transform: translateY(20px);
+  }
 `;
 export const Container = styled.div`
   width: 100%;
@@ -19,16 +36,17 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  z-index: 1;
 `;
 const LogoContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 20%;
   margin: 4rem;
-  transition: opacity 1.3s ease, transform 0.8s ease-in-out;
-  opacity: ${({ animate }) => animate.visibility};
-  transform: ${({ animate }) => `translateY(${animate.move})`};
+  transition: opacity 1.3s ease, transform 0.9s ease-in-out;
+  opacity: ${({ animate }) => (animate ? animate.visibility : 1)};
+  transform: ${({ animate }) =>
+    animate ? `translateY(${animate.move})` : "translateY(0px)"};
 `;
 const Html = styled(HtmlLogo)`
   height: 2rem;
@@ -48,21 +66,24 @@ const ReactLogoStyled = styled(ReactLogo)`
 `;
 
 function About() {
-  const [show, setShow] = useState({ move: "20px", visibility: 0 });
-  const handleShow = () => {
-    return setShow({ move: "0px", visibility: 1 });
-  };
+  const [show, setShow] = useState(hide);
   useEffect(() => {
-    handleShow();
+    setShow(animateShow);
+    return () => {
+      setShow(hide);
+    };
   }, []);
 
   return (
     <Container>
-      <Heading animate={show} className="md:text-3xl text-3xl">
+      <Heading
+        animate={show}
+        className="md:text-3xl text-3xl font-nunitoBold font-semibold"
+      >
         Creator din loves designs and passionate <br /> about developing using
         these tools.
       </Heading>
-      <LogoContainer animate={show}>
+      <LogoContainer animate={show} className="lg:w-3/12 sm:w-4/12 w-7/12">
         <Html />
         <Js />
         <Tailwind />
