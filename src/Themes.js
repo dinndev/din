41af642cb1319b2d389f68a591394534e569de32
dinndev <dinndev@gmail.com>
2021-02-09 17:54:13 +0800

    Themes and colors

--- src/Themes.js
+++ src/Themes.js
@@ -8,3 +8,9 @@ export const darkTheme = {
   background: "#666565",
   color: "white",
 };
+
+export const animateShow = { move: "0px", visibility: 1 };
+export const hide = { move: "20px", visibility: 0 };
+
+export const animate = { move: 0, visibility: 1 };
+export const animateHide = { move: 20, visibility: 0 };
